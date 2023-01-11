056c5b931eb0587c41181104e1a8a1ed9b15da3b
dberry38 <108836058+dberry38@users.noreply.github.com>
2023-01-11 13:58:44 -0800

    Remove minicssextractplugin; add injectmanifest and webpackpwamanifest; update requires.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,8 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 // Require the GenerateSW class of the WorkBoxPlugin 
-const WorkboxPlugin = require('workbox-webpack-plugin');
 const path = require('path');
+const { InjectManifest, WorkboxPlugin } = require('workbox-webpack-plugin');
 
 
 module.exports = () => {
@@ -17,33 +17,34 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+            // Injects our custom service worker
+            new InjectManifest({
+              swSrc: './src-sw.js',
+              swDest: 'src-sw.js',
+            }),
+      
+            // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Webpack Plugin',
-      }),
-      new MiniCssExtractPlugin(),
-      new WorkboxPlugin.GenerateSW({
-        // Do not precache images
-        exclude: [/\.(?:png|jpg|jpeg|svg)$/],
-  
-        // Define runtime caching rules.
-        runtimeCaching: [{
-          // Match any request that ends with .png, .jpg, .jpeg or .svg.
-          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-  
-          // Apply a cache-first strategy.
-          handler: 'CacheFirst',
-  
-          options: {
-            // Use a custom cache name.
-            cacheName: 'images',
-  
-            // Only cache 2 images.
-            expiration: {
-              maxEntries: 2,
-            },
-          },
-        }],
+        title: 'JATE',
       }),
     ],
 
@@ -51,22 +52,20 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, 'css-loader'],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
+          use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
-        },  
+        },
       ],
     },
   };
